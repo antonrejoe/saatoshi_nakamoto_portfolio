badf3ec177a41d53baf172d369ae24b0ca90c051
anton_rejoe <antonrejoe@gmail.com>
2022-08-22 18:28:05 +0530

    nav-bar slides :P

--- index.js
+++ index.js
@@ -36,9 +36,14 @@ function animateHamburger()
         document.querySelector('.hamburger.mid').classList.remove('hide');
         document.querySelector('.hamburger.above').classList.remove('rot_up');
         document.querySelector('.hamburger.below').classList.remove('rot_down');
-        document.querySelector('._hamburger').classList.add('rotate_720');
-       
+        document.querySelector('._hamburger').classList.remove('rotate_720');
+        // document.getElementById('._hamburger').addEventListener('mouseover',function () {
         
+        //     document.getElementById('._hamburger').style.backgroundColor='red';
+            
+        // },function () {
+        //     document.getElementById('._hamburger').style.backgroundColor='white';
+        // });
        
        
         return  state_of_burger -= 1
@@ -82,19 +87,23 @@ function visibleNavar() {
  const nav_bar =  document.querySelector('li.menu-list-item1.active')
 
  var $zero_view = '0vh' ;
-var $full_view = '-100vh' ;
-
+var $full_view = '-100vw' ;
+var $move_ryt = '100vw';
+var $appear = '0vw';
 function show_navbar()
 {
 
     if($nav_bar_closed === 'hidden' ){
         document.querySelector( 'li.menu-list-item1.active' ).style.visibility = $nav_bar_open;
-       
+        document.querySelector( 'li.menu-list-item1.active' ).style.opacity = '0.95';
+        document.querySelector( 'li.menu-list-item1.active' ).style.transform ='translateX(0vw)' ;
         return $nav_bar_closed = visible 
     }
-        if($nav_bar_closed === visible  ){
+        if($nav_bar_closed === visible  )
+        {
         document.querySelector( 'li.menu-list-item1.active' ).style.visibility = hidden;
-        
+        document.querySelector( 'li.menu-list-item1.active' ).style.opacity = '0';
+        document.querySelector( 'li.menu-list-item1.active' ).style.transform ='translateX(100vw)'
         return $nav_bar_closed = hidden 
     }
    
