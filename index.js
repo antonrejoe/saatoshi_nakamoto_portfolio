9f056287b6405d77bd6696d295d84eaeea258bdc
anton_rejoe <antonrejoe@gmail.com>
2022-08-24 10:06:44 +0530

    working with the navbar items
      pick cafe872 working with the navbar items
        squash 2bbd97e to rebase

--- index.js
+++ index.js
@@ -100,6 +100,9 @@ function show_navbar()
         document.querySelector( 'li.menu-list-item1.active' ).style.visibility = $nav_bar_open;
         document.querySelector( 'li.menu-list-item1.active' ).style.opacity = '0.95';
         document.querySelector( 'li.menu-list-item1.active' ).style.transform ='translateX(0vw)' ;
+
+       
+
         return $nav_bar_closed = visible 
     }
         if($nav_bar_closed === visible  )
@@ -120,6 +123,48 @@ function show_navbar()
 function rotate_burger() {
     
     document.querySelector('._hamburger').classList.toggle('hamburger-rot');
+    
+    
 }
+
+
+function selector(name) {
+    const x = document.querySelectorAll(name);
+    return x;
+
+
+}
+
+
+// my_links = [];
+
+// my_links.push(selector('.menu-link'));
+
+// console.log(selector('.menu-link'));
+
+
+// console.log(my_links[0]);
+for (let i = 0; i <4; i++){
+
+    // const my_links_present = [];
+    // my_links_present.push( my_links[i]); ///this one is not useful coz' the 
+    ///⬆️⬆️<my_links walla> the above commented code selects the a.menu-link altogether <it gives an array of a tags>
+
+    // console.log(my_links[i]);
+
+
+    const a_content = '.menu-link' +'.' + 'a' +(i+1) ;
+    
+    
+    const check =   selector(a_content)  ;
+    // console.log(check);
+    // console.log(a_content);
+
+   
+
+    
+
+}
+
 
 
